8dd91e8f5178b11f7b90d3c92e66430eedef14af
Merlin Moelter <joebau.0815@googlemail.com>
2019-07-25 18:34:29 +0200

    Move functions of event listeners in Window.js into individual methods

--- new-website/src/components/Window/Window.js
+++ new-website/src/components/Window/Window.js
@@ -9,53 +9,21 @@ export default class Window extends React.Component{
     mousedown = {header: false}
 
     componentDidMount(){
-        // Setting appearing position of window
+        // Setting initial position of window
         this.window.style.left = Math.random() * (window.innerWidth - winStartWidth) + "px"
         this.window.style.top = Math.random() * (window.innerHeight - winStartHeight) + "px"
         this.pos = {x: this.window.offsetLeft, y: this.window.offsetTop}
         this.dimensions = {width: this.window.clientWidth, height: this.window.clientHeight}
+
         // Controls for moving window
-        this.header.addEventListener("mousedown", e => {
-            // When user wants to move screen while fullscreen is open -> close fullscreen
-            if(this.state.expanded){
-                this.setState({expanded: false})
-                // Set window to same position on the x axis as the user has clicked it while it was in fullscreen mode
-                this.window.style.left = e.clientX - (e.clientX/window.innerWidth*this.window.clientWidth) + "px"
-                this.window.style.top = e.clientY + "px"
-            }
-            this.oldMousePos = {x: e.clientX, y: e.clientY}
-            this.mousedown.header = true
-        })
-        this.header.addEventListener("mouseup", () => this.mousedown.header = false)
-        document.addEventListener("mousemove", e => {
-            if(this.mousedown.header){
-                const x = this.oldMousePos.x - e.clientX
-                const y = this.oldMousePos.y - e.clientY
-
-                this.window.style.left = this.window.offsetLeft - x + "px"
-                this.window.style.top = this.window.offsetTop - y + "px"
-
-                this.oldMousePos = {x: e.clientX, y: e.clientY}
-                this.pos = {x: this.window.offsetLeft, y: this.window.offsetTop}
-            }
-        })
+        this.header.addEventListener("mousedown", this.handleHeaderMouseDown)
+        this.header.addEventListener("mouseup", this.handleHeaderMouseUp)
+        document.addEventListener("mousemove", this.handleHeaderMouseMove)
 
         // Controls for resizing window
-        this.resize.addEventListener("mousedown", e => {
-            this.startPos = {x: e.clientX, y: e.clientY}
-            this.startDimensions = {width: this.window.offsetWidth, height: this.window.offsetHeight}
-            this.mousedown.resize = true
-        })
-        this.resize.addEventListener("mouseup", () => this.mousedown.resize = false)
-        document.addEventListener("mousemove", e => {
-            if(this.mousedown.resize && !this.state.expanded){
-                const newWidth = this.startDimensions.width + e.clientX - this.startPos.x
-                const newHeight = this.startDimensions.height + e.clientY - this.startPos.y
-                this.window.style.width = newWidth + "px"
-                this.window.style.height = newHeight + "px"
-                this.dimensions = {width: newWidth, height: newHeight}
-            }
-        })
+        this.resize.addEventListener("mousedown", this.handleResizeMouseDown)
+        this.resize.addEventListener("mouseup", this.handleResizeMouseUp)
+        document.addEventListener("mousemove", this.handleResizeMouseMove)
     }
 
     resetStyles = () => {
@@ -90,8 +58,55 @@ export default class Window extends React.Component{
             this.deactivateFullscreen()
     }
 
+    handleHeaderMouseDown = e => {
+        // When user wants to move screen while fullscreen is open -> close fullscreen
+        if(this.state.expanded){
+            this.setState({expanded: false})
+            // Set window to same position on the x axis as the user has clicked it while it was in fullscreen mode
+            this.window.style.left = e.clientX - (e.clientX/window.innerWidth*this.window.clientWidth) + "px"
+            this.window.style.top = e.clientY + "px"
+        }
+        this.oldMousePos = {x: e.clientX, y: e.clientY}
+        this.mousedown.header = true
+    }
+
+    handleHeaderMouseUp = () => this.mousedown.header = false
+
+    handleHeaderMouseMove = e => {
+        if(this.mousedown.header){
+            const x = this.oldMousePos.x - e.clientX
+            const y = this.oldMousePos.y - e.clientY
+
+            this.window.style.left = this.window.offsetLeft - x + "px"
+            this.window.style.top = this.window.offsetTop - y + "px"
+
+            this.oldMousePos = {x: e.clientX, y: e.clientY}
+            this.pos = {x: this.window.offsetLeft, y: this.window.offsetTop}
+        }
+    }
+
+    handleResizeMouseDown = e => {
+        this.startPos = {x: e.clientX, y: e.clientY}
+        this.startDimensions = {width: this.window.offsetWidth, height: this.window.offsetHeight}
+        this.mousedown.resize = true
+    }
+
+    handleResizeMouseUp = () => this.mousedown.resize = false
+
+    handleResizeMouseMove = e => {
+        if(this.mousedown.resize && !this.state.expanded){
+            const newWidth = this.startDimensions.width + e.clientX - this.startPos.x
+            const newHeight = this.startDimensions.height + e.clientY - this.startPos.y
+            this.window.style.width = newWidth + "px"
+            this.window.style.height = newHeight + "px"
+            this.dimensions = {width: newWidth, height: newHeight}
+        }
+    }
+
     handleExpand = () => this.setState({expanded: true})
+
     handleCompress = () => this.setState({expanded: false})
+
     handleClose = () => this.props.onClose()
 
     render(){
