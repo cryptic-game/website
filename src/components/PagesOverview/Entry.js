e36cdfe32ad3b0cf728a7a12a7207025bf50d3f9
Merlin Moelter <joebau.0815@googlemail.com>
2019-07-26 22:45:39 +0200

    Adjust colors for mobile view

--- src/components/PagesOverview/Entry.js
+++ src/components/PagesOverview/Entry.js
@@ -27,7 +27,7 @@ export default class Entry extends React.Component{
         if(this.state.showDescription && this.props.mode === "normal")
             var style = {transform: `translate(${this.entry.dataset.translateX*this.offsetMultiplicator}px, ${this.entry.dataset.translateY*this.offsetMultiplicator}px)`}
         return(
-            <div className="pages-overview-entry" ref={ref => this.entry = ref} onClick={this.props.onClick} style={{backgroundColor: this.props.color || null}}>
+            <div className="pages-overview-entry" ref={ref => this.entry = ref} onClick={this.props.onClick} style={{backgroundColor: this.props.background || null, color: this.props.color || null}}>
                 <FontAwesomeIcon icon={icons[this.props.icon]} className="icon" size={this.props.mode === "normal" ? "3x" : this.props.mode === "small" ? "2x" : null}/>
                 {this.state.showDescription && <Description content={this.props.description} style={style}/>}
             </div>
