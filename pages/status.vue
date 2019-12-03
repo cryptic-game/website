02778417a8f9477f089d11a64411cd04fe969f56
Moritz Ruth <dev@moritz-ruth.de>
2019-12-03 18:45:42 +0100

    Add status page placeholder

---
+++ pages/status.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="status-page">
+    <div class="centered-container formatted">
+      <h1>Status</h1>
+      <span class="status-page__text">This page will be available as soon as Pre Alpha 3 is released.</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .status-page__text {
+    font-size: 2rem;
+    margin-top: 20px;
+    display: inline-block;
+  }
+</style>
+
+<script>
+  export default {
+    name: "StatusPage"
+  };
+</script>
