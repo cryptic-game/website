d0e0a67c6258cf04a2df74813b5231071116460a
Marcel Koch <koch_marcel@outlook.com>
2020-03-10 18:18:39 +0100

    Updated Team. (#50)

--- pages/team.vue
+++ pages/team.vue
@@ -4,20 +4,30 @@
     <div class="content formatted">
       <h1>Team</h1>
 
-      <h6>Project Management</h6>
-      <TeamMember github-name="Chaozz66" name="Chaozz" role="Head"></TeamMember>
-      <TeamMember github-name="DevMimas" name="Marius"></TeamMember>
-
-      <h6>Gamedesign</h6>
-      <TeamMember github-name="DevMimas" name="Marius" role="Head"></TeamMember>
-
       <div class="group">
-        <h6>Frontend Development</h6>
-        <TeamMember github-name="MaxiHuHe04" name="MaxiHuHe04" role="Head"></TeamMember>
-        <TeamMember github-name="MarcelCoding" name="Marcel" role="Head-Assistant"></TeamMember>
-        <TeamMember github-name="JannikEmmerich" name="Jannik"></TeamMember>
+        <h6>Team Management</h6>
+        <TeamMember github-name="Chaozz66" name="Chaozz" role="Head"></TeamMember>
+        <!--        <TeamMember github-name="DevMimas" name="Marius"></TeamMember>-->
       </div>
 
+      <div class="group">
+        <h6>Gamedesign</h6>
+        <TeamMember github-name="DevMimas" name="Marius" role="Head"></TeamMember>
+      </div>
+
+      <div class="group">
+        <h6>Development</h6>
+        <TeamMember github-name="Defelo" name="Defelo" role="Head of Development"></TeamMember>
+        <TeamMember github-name="TheCataliasTNT2k" name="TNT2k" role="Backend Head"></TeamMember>
+        <TeamMember github-name="JannikEmmerich" name="Jannik" role="Backend Head Assistant"></TeamMember>
+        <TeamMember github-name="MaxiHuHe04" name="MaxiHuHe04" role="Frontend Head"></TeamMember>
+        <TeamMember github-name="MarcelCoding" name="Marcel" role="Frontend Head Assistant"></TeamMember>
+      </div>
+
+      <div class="group">
+        <h6>Design</h6>
+        <TeamMember github-name="Chaozz66" name="Chaozz" role="Head"></TeamMember>
+      </div>
     </div>
   </main>
 </template>
