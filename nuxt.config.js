3cbf2bbb04280fed787c65a822cc7f5aa67abb3e
Marcel Koch <koch_marcel@outlook.com>
2020-03-11 21:32:14 +0100

    Added 404 Page. (57)

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,16 @@ export default {
     "@nuxtjs/pwa"
   ],
 
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: 'custom',
+        path: '*',
+        component: resolve(__dirname, 'pages/not-found.vue')
+      })
+    }
+  },
+
   kiste: {
     theme: {
       colors: {
