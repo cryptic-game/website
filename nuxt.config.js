abe1db3bc8f3d5205f3cd7cec8a1fbdaf6d1c62c
Marcel Koch <koch_marcel@outlook.com>
2020-04-10 16:40:26 +0200

    Added Open Graph Tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   mode: "universal",
 
   head: {
-    title: "Cryptic",
+    title: "Cryptic Game",
     meta: [
       {charset: "utf-8"},
       {name: "viewport", content: "width=device-width, initial-scale=1"},
