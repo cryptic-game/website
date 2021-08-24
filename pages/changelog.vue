ffee10681b588adadb3e89635f38a6977bf88984
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-08-24 09:05:57 +0200

    Added Title and Fetch routine for changelog page

--- pages/changelog.vue
+++ pages/changelog.vue
@@ -5,7 +5,18 @@
       <h1>{{ $t("navbar.changelog") }}</h1>
 
       <div class="grid">
-        
+        <div class="versionEntry" v-for="change in changes.versions" :key="change.name">
+          <article>
+          <div>
+            <h3>{{change.name}}</h3>
+            <h4>{{change.date}}</h4>
+          
+          <div/>
+          <div>{{change.enhancements}}</div>
+          <div>{{change.fixes}}</div>
+        </article>
+        <br/>
+        </div>
       </div>
     </div>
   </main>
@@ -20,6 +31,16 @@ export default {
     
     NavigationBar
   },
+  async fetch() {
+  const response = await fetch("https://play.cryptic-game.net/assets/changelog.json");
+  const data = await response.json();
+  this.changes = data;
+  },
+  data () {
+    return {
+      changes: [],
+    }
+  },
   head () {
     return {
       titleTemplate: 'Changelog - %s'
@@ -30,5 +51,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.versionEntry{
+  background: rgb(26, 26, 26);
+  color: rgb(235, 227, 184);
+  padding: 20px;
+  width: 100%;
+}
 
 </style>
