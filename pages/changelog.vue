5394692410c0ac722d1e4cdbd7fa2793f80b9a42
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-08-24 08:27:30 +0200

    added changelog page
    Added changelog page and added changelog page to navbar.

---
+++ pages/changelog.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="changelog-page">
+    <NavigationBar title="Changelog" />
+    <div class="content formatted">
+      <h1>{{ $t("navbar.changelog") }}</h1>
+
+      <div class="grid">
+        
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar'
+
+export default {
+  name: 'ChangelogPage',
+  components: {
+    
+    NavigationBar
+  },
+  head () {
+    return {
+      titleTemplate: 'Changelog - %s'
+    }
+  },
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
