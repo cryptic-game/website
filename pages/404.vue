25e7f18a9a12d25f43a287ea95ce23b18711612b
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-08-28 14:25:14 +0000

    Readded 404 page

---
+++ pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="team-page">
+    <NavigationBar title="Not found" />
+    <div class="content formatted">
+      <h1>Not found</h1>
+      <p class="heading--5">
+        The page you requested could not be found.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar'
+
+export default {
+  name: 'NotFound',
+  components: { NavigationBar }
+}
+</script>
+
+<style scoped>
+
+</style>
