305211d7c8a1e3f1406e761020cf25d4b1aacf87
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-09-07 20:47:59 +0200

    Added Changelog Page (#227)
    - changelog page
    - navbar
    - query data from https://play.cryptic-game.net/assets/changelog.json
    - improved i18n
    
    Co-authored-by: Marcel <34819524+MarcelCoding@users.noreply.github.com>

---
+++ pages/changelog.vue
@@ -0,0 +1,108 @@
+<template>
+  <main class="changelog-page">
+    <NavigationBar title="Changelog" />
+    <div class="content formatted">
+      <h1>{{ $t("navbar.changelog") }}</h1>
+
+      <div class="grid">
+        <div v-for="change in changes.versions" :key="change.name" class="versionEntry">
+          <article>
+            <div>
+              <h3>{{ change.name }}</h3>
+              <client-only>
+                <small>{{ getPrettyDate(change.date) }}</small>
+              </client-only>
+              <div />
+
+              <div class="enhancements">
+                <h4>{{ $t("changelog.enhancements") }}</h4>
+
+                <div v-for="enhancement in change.enhancements" :key="enhancement" class="enhancement">
+                  <h5 style="font-weight: normal;">
+                    {{ enhancement }}
+                  </h5>
+                </div>
+              </div>
+
+              <div class="fixes">
+                <h4>{{ $t("changelog.fixes") }}</h4>
+
+                <div v-for="fix in change.fixes" :key="fix" class="fix">
+                  <h5 style="font-weight: normal;">
+                    {{ fix }}
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </article>
+          <br>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar'
+export default {
+  name: 'ChangelogPage',
+  components: {
+
+    NavigationBar
+  },
+  data () {
+    return {
+      changes: []
+    }
+  },
+  async fetch () {
+    const response = await fetch('https://play.cryptic-game.net/assets/changelog.json')
+    this.changes = await response.json()
+  },
+  head () {
+    return {
+      titleTemplate: 'Changelog - %s'
+    }
+  },
+  methods: {
+    getPrettyDate (date) {
+      return new Intl.DateTimeFormat(this.$i18n.localeProperties.iso).format(new Date(date))
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .fixes{
+    margin-top: 10px;
+    border-style: none;
+    border-radius: 8px;
+    color: rgb(221, 71, 44);
+  }
+  .enhancements{
+    margin-top: 10px;
+    border-style: none;
+    border-radius: 8px;
+    color: rgb(79, 185, 204);
+  }
+  .fix{
+    margin-top: 5px;
+    color: white;
+
+  }
+  .enhancement{
+    color: white;
+    margin-top: 5px;
+  }
+.versionEntry{
+  color: white;
+  padding: 20px;
+  width: 100%;
+  //background: linear-gradient(to right,black,black,black,#3EAB34);
+  border-left-style: solid;
+  border-left-width: 14px;
+  border-left-color: #3EAB34;
+}
+
+</style>
