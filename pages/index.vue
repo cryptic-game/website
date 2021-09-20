a08e1130e6672d14479b5f6da346c8ca20e7cbb7
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-09-20 17:16:24 +0200

    Update pages/index.vue
    Co-authored-by: Marcel <34819524+MarcelCoding@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -302,7 +302,7 @@ export default {
     const lang = this.$i18n.locale
     const response = await fetch('https://api.admin.staging.cryptic-game.net/website/blog/' + lang)
     let posts = await response.json()
-    posts = posts.reverse().slice(-2).reverse()
+    posts = posts.slice(0, 2)
     this.blogPosts = posts
   },
   created () {
