bdc86b8272f82b53fed05ed6d7641578cabd9533
ClientCrash <clientcrashyt@gmail.com>
2021-09-25 17:29:18 +0200

    added message of the day component and read more button

---
+++ components/MessageOfTheDay.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="motd" :style="motd.style">
+    {{ motd.message }} <a v-if="motd.readmorelink != undefined" :href="motd.readmorelink" :style="motd.style"> READ MORE </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'Motd',
+  components: {},
+  data: () => ({
+    motd: {},
+    style: {}
+  }),
+  fetch () {
+    this.motd = { message: 'Cryptic wird heute zwei jahre alt! 🥳', readmorelink: 'https://google.de' }
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+.motd{
+    width: fit-content;
+    padding: 0.4vw;
+    margin-right: 10%;
+    margin-left: 4%;
+    text-align: center;
+    background: #34A735;
+    border-radius: 4px;
+    margin-bottom: 2vh;
+}
+a{
+    color: white;
+    background: #1D1C1C;
+    text-decoration: none;
+    border-radius: 8px;
+    padding: 4px;
+    font-size: 0.8rem;
+}
+</style>
